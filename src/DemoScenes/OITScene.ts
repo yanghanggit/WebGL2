3b46370e5a0d94b2dc6707de43f4621350e7fa20
yang hang <1220826563@qq.com>
2019-06-29 19:09:06 +0800

    添加资源

--- src/DemoScenes/OITScene.ts
+++ src/DemoScenes/OITScene.ts
@@ -159,7 +159,7 @@ class OITScene extends WebGL2DemoScene {
             this.accumProgram = programs[0];
             this.blendProgram = programs[1];
             //
-            const images = await this.engine.loadImages(["resource/assets/bg.jpg"]);
+            const images = await this.engine.loadImages(["resource/assets/webgl-logo.png"]);
             this.image = images[0];
         }
         catch (e) {
--- src/Main.ts
+++ src/Main.ts
@@ -75,6 +75,7 @@ function getQualifiedClassName(value: any): string {
 
 /////////
 const __SceneClasses__ = [
+    TextureArrayScene,
     TriangleScene,
     UBOScene,
     OITScene,
