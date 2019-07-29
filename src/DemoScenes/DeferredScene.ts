8804bcbde9df018171463a4441920c46de92ee01
yang hang <1220826563@qq.com>
2019-07-29 16:50:17 +0800

    optimize deferred scene code

--- src/DemoScenes/DeferredScene.ts
+++ src/DemoScenes/DeferredScene.ts
@@ -1,6 +1,24 @@
 
+interface DeferredScnBoxTransform {
+    scale: Float32Array | number[];
+    rotate: Float32Array | number[];
+    translate: Float32Array | number[];
+    modelMatrix: Float32Array;
+    mvpMatrix: Float32Array;
+    matrixUniforms: WebGL2UniformBuffer;
+    drawCall: WebGL2DrawCall;
+}
+
+interface DeferredScnLightTransform {
+    position: Float32Array;
+    color: Float32Array;
+    uniforms: WebGL2UniformBuffer;
+    drawCall: WebGL2DrawCall;
+}
+
 class DeferredScene extends WebGL2DemoScene {
 
+    //
     private projMatrix: Float32Array;
     private viewProjMatrix: Float32Array;
     private viewMatrix: Float32Array;
@@ -8,10 +26,10 @@ class DeferredScene extends WebGL2DemoScene {
     private geoFsSource: string;
     private vsSource: string;
     private fsSource: string;
-    private lights: any[];
-    private boxes: any[];
+    private lights: DeferredScnLightTransform[];
+    private boxes: DeferredScnBoxTransform[];
     private image: HTMLImageElement;
-
+    //
     private geoProgram: WebGL2Program;
     private mainProgram: WebGL2Program;
     private gBuffer: WebGL2Framebuffer;
@@ -36,108 +54,62 @@ class DeferredScene extends WebGL2DemoScene {
     }
 
     private createScene(): void {
+
         const engine = this.engine;
-
-        const app = engine;
-        const PicoGL = GL;
-        const utils = engine;
-        const canvas = engine.canvas;
-
-
-
-        // let canvas = document.getElementById("gl-canvas");
-        // canvas.width = window.innerWidth;
-        // canvas.height = window.innerHeight;
-
-        //let app = PicoGL.createApp(canvas)
-        app
-            .clearColor(0.0, 0.0, 0.0, 1.0)
+        engine.clearColor(0.0, 0.0, 0.0, 1.0)
             .depthTest()
-            .depthFunc(PicoGL.LEQUAL)
-            .blendFunc(PicoGL.ONE, PicoGL.ONE);
+            .depthFunc(GL.LEQUAL)
+            .blendFunc(GL.ONE, GL.ONE);
 
-        // let timer = app.createTimer();
-
-        // // SET UP GBUFFER PROGRAM
-        // let geoVsSource =  document.getElementById("geo-vs").text.trim();
-        // let geoFsSource =  document.getElementById("geo-fs").text.trim();
-
-        let positionTarget = app.createTexture2DBySize(app.width, app.height, {
-            internalFormat: PicoGL.RGBA16F
+        const positionTarget = engine.createTexture2DBySize(engine.width, engine.height, {
+            internalFormat: GL.RGBA16F
         });
-        let normalTarget = app.createTexture2DBySize(app.width, app.height, {
-            internalFormat: PicoGL.RGBA16F
+        const normalTarget = engine.createTexture2DBySize(engine.width, engine.height, {
+            internalFormat: GL.RGBA16F
         });
-        let uvTarget = app.createTexture2DBySize(app.width, app.height, {
-            internalFormat: PicoGL.RG16F
+        const uvTarget = engine.createTexture2DBySize(engine.width, engine.height, {
+            internalFormat: GL.RG16F
         });
-        let depthTarget = app.createRenderbuffer(app.width, app.height, PicoGL.DEPTH_COMPONENT16);
-        this.gBuffer = app.createFramebuffer()
+        const depthTarget = engine.createRenderbuffer(engine.width, engine.height, GL.DEPTH_COMPONENT16);
+        this.gBuffer = engine.createFramebuffer()
             .colorTarget(0, positionTarget)
             .colorTarget(1, normalTarget)
             .colorTarget(3, uvTarget)
             .depthTarget(depthTarget);
-        console.assert(this.gBuffer.getStatus() === PicoGL.FRAMEBUFFER_COMPLETE, "G-buffer framebuffer is not complete!");
+        console.assert(this.gBuffer.getStatus() === GL.FRAMEBUFFER_COMPLETE, "G-buffer framebuffer is not complete!");
 
-        // SET UP MAIN PROGRAM
-        // let vsSource =  document.getElementById("main-vs").text.trim();
-        // let fsSource =  document.getElementById("main-fs").text.trim();
-
-        // GEOMETRY
-        let box = utils.createBox();
-        let positions = app.createVertexBuffer(PicoGL.FLOAT, 3, box.positions);
-        let normals = app.createVertexBuffer(PicoGL.FLOAT, 3, box.normals);
-        let uv = app.createVertexBuffer(PicoGL.FLOAT, 2, box.uvs);
-
-        let boxArray = app.createVertexArray()
+        ///
+        const box = engine.createBox();
+        const positions = engine.createVertexBuffer(GL.FLOAT, 3, box.positions);
+        const normals = engine.createVertexBuffer(GL.FLOAT, 3, box.normals);
+        const uv = engine.createVertexBuffer(GL.FLOAT, 2, box.uvs);
+        const boxArray = engine.createVertexArray()
             .vertexAttributeBuffer(0, positions)
             .vertexAttributeBuffer(1, normals)
             .vertexAttributeBuffer(2, uv);
 
-        let sphere = utils.createSphere();
-        let lightPositions = app.createVertexBuffer(PicoGL.FLOAT, 3, sphere.positions);
-        let lightIndices = app.createIndexBuffer(PicoGL.UNSIGNED_SHORT, 3, sphere.indices);
-
-        let sphereArray = app.createVertexArray()
+        const sphere = engine.createSphere();
+        const lightPositions = engine.createVertexBuffer(GL.FLOAT, 3, sphere.positions);
+        const lightIndices = engine.createIndexBuffer(GL.UNSIGNED_SHORT, 3, sphere.indices);
+        const sphereArray = engine.createVertexArray()
             .vertexAttributeBuffer(0, lightPositions)
             .indexBuffer(lightIndices);
 
-        // CAMERA STUFF
         this.projMatrix = mat4.create();
-        mat4.perspective(this.projMatrix, Math.PI / 2, canvas.width / canvas.height, 0.1, 10.0);
+        mat4.perspective(this.projMatrix, Math.PI / 2, engine.canvas.width / engine.canvas.height, 0.1, 10.0);
 
         this.viewMatrix = mat4.create();
-        let eyePosition = vec3.fromValues(1, 1, 1);
+        const eyePosition = vec3.fromValues(1, 1, 1);
         mat4.lookAt(this.viewMatrix, eyePosition, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
 
         this.viewProjMatrix = mat4.create();
         mat4.multiply(this.viewProjMatrix, this.projMatrix, this.viewMatrix);
 
-        /*
-        window.onresize = function() {
-            app.resize(window.innerWidth, window.innerHeight);
-            gBuffer.resize();
-
-            mat4.perspective(projMatrix, Math.PI / 2, app.canvas.width / app.canvas.height, 0.1, 10.0);
-            mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
-
-            for (let i = 0, len = lights.length; i < len; ++i) {
-                lightMatrix = mat4.create();
-                utils.xformMatrix(lightMatrix, lights[i].position);
-                mat4.multiply(lightMatrix, viewProjMatrix, lightMatrix);
-
-
-                lights[i].uniforms.set(0, lightMatrix).update();
-            }
-        }
-        */
-
-        let texture = app.createTexture2DByImage(this.image, {
+        const texture = engine.createTexture2DByImage(this.image, {
             flipY: true,
-            maxAnisotropy: app.capbility('MAX_TEXTURE_ANISOTROPY')//PicoGL.WEBGL_INFO.MAX_TEXTURE_ANISOTROPY
+            maxAnisotropy: engine.capbility('MAX_TEXTURE_ANISOTROPY')
         });
 
-        // BOX DESCRIPTIONS FOR GEO PASS
         this.boxes = [
             {
                 scale: [1, 1, 1],
@@ -145,9 +117,9 @@ class DeferredScene extends WebGL2DemoScene {
                 translate: [0, 0, 0],
                 modelMatrix: mat4.create(),
                 mvpMatrix: mat4.create(),
-                matrixUniforms: app.createUniformBuffer([
-                    PicoGL.FLOAT_MAT4,
-                    PicoGL.FLOAT_MAT4
+                matrixUniforms: engine.createUniformBuffer([
+                    GL.FLOAT_MAT4,
+                    GL.FLOAT_MAT4
                 ]),
                 drawCall: null
             },
@@ -157,20 +129,20 @@ class DeferredScene extends WebGL2DemoScene {
                 translate: [0.8, 0.8, 0.4],
                 modelMatrix: mat4.create(),
                 mvpMatrix: mat4.create(),
-                matrixUniforms: app.createUniformBuffer([
-                    PicoGL.FLOAT_MAT4,
-                    PicoGL.FLOAT_MAT4
+                matrixUniforms: engine.createUniformBuffer([
+                    GL.FLOAT_MAT4,
+                    GL.FLOAT_MAT4
                 ]),
                 drawCall: null
             }
         ];
+
         const boxes = this.boxes;
         for (let i = 0, len = boxes.length; i < len; ++i) {
-            boxes[i].drawCall = app.createDrawCall(this.geoProgram, boxArray)
+            boxes[i].drawCall = engine.createDrawCall(this.geoProgram, boxArray)
                 .uniformBlock("BoxUniforms", boxes[i].matrixUniforms);
         }
 
-        // LIGHT DESCRIPTIONS FOR DRAW PASS
         this.lights = [
             {
                 position: vec3.fromValues(0, 1, 0.5),
@@ -198,25 +170,22 @@ class DeferredScene extends WebGL2DemoScene {
             }
         ];
 
-        // LIGHT UNIFORMS AND DRAW CALLS    
-        let lightMatrix;
         const lights = this.lights;
         for (let i = 0, len = lights.length; i < len; ++i) {
-            lightMatrix = mat4.create();
-            utils.xformMatrix(lightMatrix, lights[i].position);
+            const lightMatrix = mat4.create();
+            engine.xformMatrix(lightMatrix, lights[i].position);
             mat4.multiply(lightMatrix, this.viewProjMatrix, lightMatrix);
 
-            lights[i].uniforms = app.createUniformBuffer([
-                PicoGL.FLOAT_MAT4,
-                PicoGL.FLOAT_VEC4,
-                PicoGL.FLOAT_VEC4
-            ])
-                .set(0, lightMatrix)
+            lights[i].uniforms = engine.createUniformBuffer([
+                GL.FLOAT_MAT4,
+                GL.FLOAT_VEC4,
+                GL.FLOAT_VEC4
+            ]).set(0, lightMatrix)
                 .set(1, lights[i].position)
                 .set(2, lights[i].color)
                 .update();
 
-            lights[i].drawCall = app.createDrawCall(this.mainProgram, sphereArray)
+            lights[i].drawCall = engine.createDrawCall(this.mainProgram, sphereArray)
                 .uniformBlock("LightUniforms", lights[i].uniforms)
                 .uniform("uEyePosition", eyePosition)
                 .texture("uTextureMap", texture)
@@ -262,26 +231,20 @@ class DeferredScene extends WebGL2DemoScene {
         if (!this._ready) {
             return;
         }
-        const app = this.engine;
-        // this.drawCall.uniform("uTime", (performance.now() - this.startTime) / 1000);
-        // this.engine.clear();
-        // this.drawCall.draw();
-        const utils = this.engine;
+        const engine = this.engine;
         const boxes = this.boxes;
         for (let i = 0, len = boxes.length; i < len; ++i) {
             boxes[i].rotate[0] += 0.01;
             boxes[i].rotate[1] += 0.02;
-
-            utils.xformMatrix(boxes[i].modelMatrix, boxes[i].translate, boxes[i].rotate, boxes[i].scale);
+            engine.xformMatrix(boxes[i].modelMatrix, boxes[i].translate as Float32Array, boxes[i].rotate as Float32Array, boxes[i].scale as Float32Array);
             mat4.multiply(boxes[i].mvpMatrix, this.viewProjMatrix, boxes[i].modelMatrix);
-
             boxes[i].matrixUniforms.set(0, boxes[i].mvpMatrix)
                 .set(1, boxes[i].modelMatrix)
                 .update();
         }
 
-        // DRAW TO GBUFFER
-        app.drawFramebuffer(this.gBuffer)
+        engine.drawFramebuffer(this.gBuffer)
+            .clearColor(0.0, 0.0, 0.0, 1.0)
             .depthMask(true)
             .noBlend()
             .clear();
@@ -290,48 +253,32 @@ class DeferredScene extends WebGL2DemoScene {
             boxes[i].drawCall.draw();
         }
 
-        // DRAW LIGHTS
-        app.defaultDrawFramebuffer()
+        engine.defaultDrawFramebuffer()
+            .clearColor(0.0, 0.0, 0.0, 1.0)
             .blend()
             .depthMask(false)
             .clear();
+
         const lights = this.lights;
         for (let i = 0, len = lights.length; i < len; ++i) {
             lights[i].drawCall.draw();
         }
-
         return this;
     }
 
     public leave(): WebGL2DemoScene {
-
-
-        // private geoProgram: WebGL2Program;
-        // private mainProgram: WebGL2Program;
-        // private gBuffer: WebGL2Framebuffer;
-
         this.geoProgram.delete();
         this.mainProgram.delete();
         this.gBuffer.delete();
-        // this.program.delete();
-        // this.drawCall.delete();
         const engine = this.engine;
         engine.noDepthTest().noBlend();
-
-
-      
-
         return this;
     }
 
     public resize(width: number, height: number): WebGL2DemoScene {
-        // mat4.perspective(this.projMatrix, Math.PI / 2, width / height, 0.1, 10.0);
-        // mat4.multiply(this.mvpMatrix, this.projMatrix, this.viewMatrix);
-
         const engine = this.engine;
-
+        
         this.gBuffer.resize();
-
         mat4.perspective(this.projMatrix, Math.PI / 2, width / height, 0.1, 10.0);
         mat4.multiply(this.viewProjMatrix, this.projMatrix, this.viewMatrix);
         let lightMatrix: Float32Array;
@@ -340,8 +287,6 @@ class DeferredScene extends WebGL2DemoScene {
             lightMatrix = mat4.create();
             engine.xformMatrix(lightMatrix, lights[i].position);
             mat4.multiply(lightMatrix, this.viewProjMatrix, lightMatrix);
-
-
             lights[i].uniforms.set(0, lightMatrix).update();
         }
         return this;
