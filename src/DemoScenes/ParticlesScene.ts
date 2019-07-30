707e9413a84fcfb3a5b9be22cfb05fc943e76135
yang hang <1220826563@qq.com>
2019-07-30 11:18:15 +0800

    optimize code of particles scene

--- src/DemoScenes/ParticlesScene.ts
+++ src/DemoScenes/ParticlesScene.ts
@@ -26,37 +26,16 @@ class ParticlesScene extends WebGL2DemoScene {
 
     private createScene(): void {
         const engine = this.engine;
-        const app = this.engine;
-        const utils = this.engine;
-        const PicoGL = GL;
-        const canvas = this.engine.canvas;
-
-
-        // utils.addTimerElement();
-
-        // let canvas = document.getElementById("gl-canvas");
-        // canvas.width = window.innerWidth;
-        // canvas.height = window.innerHeight;
-
-        //let app = PicoGL.createApp(canvas)
-        app
-            .clearColor(0.0, 0.0, 0.0, 1.0)
+        engine.clearColor(0.0, 0.0, 0.0, 1.0)
             .blend()
-            .blendFunc(PicoGL.ONE, PicoGL.ONE_MINUS_SRC_ALPHA);
+            .blendFunc(GL.ONE, GL.ONE_MINUS_SRC_ALPHA);
 
-        // let timer = app.createTimer();
-
-        // // PROGRAM
-        // let vsSource = document.getElementById("main-vs").text.trim();
-        // let fsSource = document.getElementById("main-fs").text.trim();
-
-        // GEOMETRY DATA
         const NUM_PARTICLES = 200000;
-        let positionData = new Float32Array(NUM_PARTICLES * 3);
-        let colorData = new Uint8Array(NUM_PARTICLES * 3);
+        const positionData = new Float32Array(NUM_PARTICLES * 3);
+        const colorData = new Uint8Array(NUM_PARTICLES * 3);
 
         for (let i = 0; i < NUM_PARTICLES; ++i) {
-            let vec3i = i * 3;
+            const vec3i = i * 3;
 
             positionData[vec3i] = Math.random() * 2 - 1;
             positionData[vec3i + 1] = Math.random() * 2 - 1;
@@ -67,43 +46,37 @@ class ParticlesScene extends WebGL2DemoScene {
             colorData[vec3i + 2] = Math.floor(Math.random() * 256);
         }
 
-        // INPUT AND OUTPUT POSITION AND VELOCITY BUFFERS
-        let positions1 = app.createVertexBuffer(PicoGL.FLOAT, 3, positionData);
-        let velocities1 = app.createVertexBuffer(PicoGL.FLOAT, 3, positionData.length);
-        let positions2 = app.createVertexBuffer(PicoGL.FLOAT, 3, positionData);
-        let velocities2 = app.createVertexBuffer(PicoGL.FLOAT, 3, positionData.length);
+        const positions1 = engine.createVertexBuffer(GL.FLOAT, 3, positionData);
+        const velocities1 = engine.createVertexBuffer(GL.FLOAT, 3, positionData.length);
+        const positions2 = engine.createVertexBuffer(GL.FLOAT, 3, positionData);
+        const velocities2 = engine.createVertexBuffer(GL.FLOAT, 3, positionData.length);
+        const colors = engine.createVertexBuffer(GL.UNSIGNED_BYTE, 3, colorData);
 
-        // COLOR BUFFER NOT INVOLVED IN TRANSFORM FEEDBACK
-        let colors = app.createVertexBuffer(PicoGL.UNSIGNED_BYTE, 3, colorData);
-
-        // INPUT AND OUTPUT VERTEX ARRAYS
-        let vertexArray1 = app.createVertexArray()
+        const vertexArray1 = engine.createVertexArray()
             .vertexAttributeBuffer(0, positions1)
             .vertexAttributeBuffer(1, velocities1)
             .vertexAttributeBuffer(2, colors, { normalized: true });
 
-        let vertexArray2 = app.createVertexArray()
+        const vertexArray2 = engine.createVertexArray()
             .vertexAttributeBuffer(0, positions2)
             .vertexAttributeBuffer(1, velocities2)
             .vertexAttributeBuffer(2, colors, { normalized: true });
 
-        // TRANSFORM FEEDBACK OBJECTS
-        let transformFeedback1 = app.createTransformFeedback()
+        const transformFeedback1 = engine.createTransformFeedback()
             .feedbackBuffer(0, positions1)
             .feedbackBuffer(1, velocities1);
 
-        let transformFeedback2 = app.createTransformFeedback()
+        const transformFeedback2 = engine.createTransformFeedback()
             .feedbackBuffer(0, positions2)
             .feedbackBuffer(1, velocities2);
 
-        // UNIFORM BUFFER FOR SIMULATION PARAMETERS
-        let massUniforms = app.createUniformBuffer([
-            PicoGL.FLOAT_VEC4,
-            PicoGL.FLOAT_VEC4,
-            PicoGL.FLOAT_VEC4,
-            PicoGL.FLOAT,
-            PicoGL.FLOAT,
-            PicoGL.FLOAT
+        const massUniforms = engine.createUniformBuffer([
+            GL.FLOAT_VEC4,
+            GL.FLOAT_VEC4,
+            GL.FLOAT_VEC4,
+            GL.FLOAT,
+            GL.FLOAT,
+            GL.FLOAT
         ]).set(0, vec3.fromValues(
             Math.random() * 2.0 - 1.0,
             Math.random() * 2.0 - 1.0,
@@ -123,14 +96,14 @@ class ParticlesScene extends WebGL2DemoScene {
             .update();
 
 
-        this.drawCall1 = app.createDrawCall(this.program, vertexArray1)
-            .primitive(PicoGL.POINTS)
+        this.drawCall1 = engine.createDrawCall(this.program, vertexArray1)
+            .primitive(GL.POINTS)
             .transformFeedback(transformFeedback2)
             .uniformBlock("Mass", massUniforms)
             .drawRanges([0, 50000]);
 
-        this.drawCall2 = app.createDrawCall(this.program, vertexArray2)
-            .primitive(PicoGL.POINTS)
+        this.drawCall2 = engine.createDrawCall(this.program, vertexArray2)
+            .primitive(GL.POINTS)
             .transformFeedback(transformFeedback1)
             .uniformBlock("Mass", massUniforms)
             .drawRanges([0, 50000]);
@@ -139,7 +112,6 @@ class ParticlesScene extends WebGL2DemoScene {
     }
 
     private async loadResource(): Promise<void> {
-
         try {
             ///
             const ress: string[] = [
@@ -164,8 +136,7 @@ class ParticlesScene extends WebGL2DemoScene {
         if (!this._ready) {
             return;
         }
-        const app = this.engine;
-        app.clear();
+        this.engine.clear();
         this.currentDrawCall.draw();
         this.currentDrawCall = this.currentDrawCall === this.drawCall1 ? this.drawCall2 : this.drawCall1;
         return this;
@@ -182,6 +153,7 @@ class ParticlesScene extends WebGL2DemoScene {
     }
 
     public resize(width: number, height: number): WebGL2DemoScene {
+        //????
         return this;
     }
 }
