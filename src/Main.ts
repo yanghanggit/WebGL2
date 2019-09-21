1fb80bb57e87b22b82fd2d404dfc78f703cb3217
yang hang <1220826563@qq.com>
2019-09-21 14:30:31 +0800

    整理资源

--- src/Main.ts
+++ src/Main.ts
@@ -75,6 +75,7 @@ function getQualifiedClassName(value: any): string {
 
 /////////
 const __SceneClasses__ = [
+    TriangleScene,
     ClothScene,
     OcclusionScene,
     MeshCompressionScene,
@@ -100,7 +101,6 @@ const __SceneClasses__ = [
     SSAOScene,
     ShadowScene,
     TextureArrayScene,
-    TriangleScene,
     UBOScene,
     OITScene,
 ];
