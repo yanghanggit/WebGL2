808aa3d701d5b4fb0ca91fc4ca1f9562b99f256e
yang hang <1220826563@qq.com>
2019-09-24 15:15:18 +0800

    整理代码

--- src/WebGL2VertexArray.ts
+++ src/WebGL2VertexArray.ts
@@ -1,4 +1,3 @@
-
 /**
  * AttributeBufferOptions
  */
@@ -12,24 +11,51 @@ interface AttributeBufferOptions {
 }
 /**
  * WebGL2VertexArray
+ * 那其实VAO主要是将一个绘制物体的各种顶点缓冲（VBO)包装一个整体.
+ * 顶点缓冲可以有顶点坐标，纹理坐标等。优点就在于能提高开发效率.
+ * 它将所有顶点绘制过程中的这些顶点的设置和绑定过程集中存储在一起.
+ * 当我们需要时，只需要使用相应的VAO即可。
+ * VAO的这种方式有点像一个中介，把所有繁琐的绑定和顶点设置工作都集中起来处理，我们需要绘制时，直接找这个中介就好了.
  */
 class WebGL2VertexArray extends WebGL2Object {
-
     /**
      * WebGLVertexArrayObject
      */
     private vertexArray: WebGLVertexArrayObject;
+    /**
+     * 
+     */
     public indexType: number = 0;
+    /**
+    * 是否是索引缓冲区
+    */
     public indexed: boolean;
+    /**
+     *
+     */
     public numElements: number = 0;
+    /**
+     *
+     */
     public numInstances: number = 1;
+    /**
+     *
+     */
     private offsets: number = 0;
+    /**
+     *
+     */
     private numDraws: number = 1;
-
+    /**
+     * constructor
+     * @param _engine 
+     */
     constructor(_engine: WebGL2Engine) {
         super(_engine);
     }
-
+    /**
+     * 重制
+     */
     public restore(): WebGL2VertexArray {
         if (this.state.vertexArray === this) {
             this.state.vertexArray = null;
@@ -40,17 +66,30 @@ class WebGL2VertexArray extends WebGL2Object {
         }
         return this;
     }
-
+    /**
+     * 对vertex buffer进行属性描述
+     * @param attributeIndex 
+     * @param vertexBuffer 
+     * @param options 
+     */
     public vertexAttributeBuffer(attributeIndex: number, vertexBuffer: WebGL2VertexBuffer, options: AttributeBufferOptions = DUMMY_OBJECT): WebGL2VertexArray {
         this.attributeBuffer(attributeIndex, vertexBuffer, options, false);
         return this;
     }
-
+    /**
+     * 对instance vertex buffer进行属性描述
+     * @param attributeIndex 
+     * @param vertexBuffer 
+     * @param options 
+     */
     public instanceAttributeBuffer(attributeIndex: number, vertexBuffer: WebGL2VertexBuffer, options: AttributeBufferOptions = DUMMY_OBJECT): WebGL2VertexArray {
         this.attributeBuffer(attributeIndex, vertexBuffer, options, true);
         return this;
     }
-
+    /**
+     * index buffer进行属性描述
+     * @param vertexBuffer 
+     */
     public indexBuffer(vertexBuffer: WebGL2VertexBuffer): WebGL2VertexArray {
         if (!this.vertexArray) {
             this.vertexArray = this.gl.createVertexArray();
@@ -62,7 +101,9 @@ class WebGL2VertexArray extends WebGL2Object {
         this.indexed = true;
         return this;
     }
-
+    /**
+     * 释放WebGL对象
+     */
     public delete(): WebGL2VertexArray {
         if (this.vertexArray) {
             this.gl.deleteVertexArray(this.vertexArray);
@@ -74,7 +115,9 @@ class WebGL2VertexArray extends WebGL2Object {
         }
         return this;
     }
-
+    /**
+     * 
+     */
     public bind(): WebGL2VertexArray {
         if (this.state.vertexArray !== this) {
             this.gl.bindVertexArray(this.vertexArray);
@@ -82,15 +125,22 @@ class WebGL2VertexArray extends WebGL2Object {
         }
         return this;
     }
-
+    /**
+     * 对一个buffer属性描述的核心实现
+     * @param attributeIndex 
+     * @param vertexBuffer 
+     * @param options 
+     * @param instanced 
+     */
     public attributeBuffer(attributeIndex: number,
         vertexBuffer: WebGL2VertexBuffer,
         options: AttributeBufferOptions = DUMMY_OBJECT,
         instanced: boolean): WebGL2VertexArray {
-            
+        //惰性初始化
         if (!this.vertexArray) {
             this.vertexArray = this.gl.createVertexArray();
         }
+        //绑定
         this.bind();
         //用vertexAttribPointer，对vertexbuffer进行描述，先bind
         this.gl.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer.buffer);
@@ -105,8 +155,10 @@ class WebGL2VertexArray extends WebGL2Object {
         const numColumns = vertexBuffer.numColumns;
         const typeSize = TYPE_SIZE[type];
         if (stride === 0) {
+            //间距
             stride = numColumns * size * typeSize;
         }
+        //
         for (let i = 0; i < numColumns; ++i) {
             if (integer) {
                 //描述开始，这里IPointer, 不考虑归一化normalized的情况
@@ -126,11 +178,14 @@ class WebGL2VertexArray extends WebGL2Object {
                     stride,
                     offset + i * size * typeSize);
             }
+            //如果是instanced
             if (instanced) {
                 this.gl.vertexAttribDivisor(attributeIndex + i, 1);
             }
+            //激活单元
             this.gl.enableVertexAttribArray(attributeIndex + i);
         }
+        //?
         if (this.numDraws === 1) {
             if (instanced) {
                 this.numInstances = vertexBuffer.numItems;
--- src/WebGL2Vertexbuffer.ts
+++ src/WebGL2Vertexbuffer.ts
@@ -1,21 +1,75 @@
-
-
+/**
+ * WebGL2VertexBuffer
+ */
+type WebGL2VertexBufferDataType = number | Float32Array | Uint16Array | Int16Array | Uint8Array | Int8Array;
+/**
+ * WebGL2VertexBuffer
+ */
 class WebGL2VertexBuffer extends WebGL2Object {
-
+    /**
+     * WebGLBuffer
+     */
     public buffer: WebGLBuffer;
+    /**
+     * 
+     */
     public readonly type: number = 0;
+    /**
+    * 
+    */
     public readonly itemSize: number = 0;
+    /**
+    * 
+    */
     public readonly numItems: number = 0;
+    /**
+    * 
+    */
     public readonly numColumns: number = 0;
+    /**
+    * 数据byte长度
+    */
     private readonly byteLength: number = 0;
+    /**
+    * 数据长度
+    */
     private readonly dataLength: number = 0;
+    /**
+    * STREAM_DRAW: 0x88E0,
+    * STATIC_DRAW: 0x88E4,
+    * DYNAMIC_DRAW: 0x88E8,
+    */
     private readonly usage: number = 0;
-    private readonly indexArray: boolean = false;
-    public readonly integer: boolean = false;
+    /**
+    * 是否是索引缓存
+    */
+    private readonly indexArray: boolean;
+    /**
+    * 是否是整形数据的buffer
+    */
+    public readonly integer: boolean;
+    /**
+    * GL.ELEMENT_ARRAY_BUFFER or GL.ARRAY_BUFFER?
+    */
     private readonly binding: number = 0;
+    /**
+     * constructor
+     * @param _engine 
+     * @param type 
+     * @param itemSize 
+     * @param data 
+     * @param usage 
+     * @param indexArray 
+     */
+    constructor(_engine: WebGL2Engine,
+        type: number,
+        itemSize: number,
+        data: WebGL2VertexBufferDataType,
+        usage: number = GL.STATIC_DRAW,
+        indexArray: boolean = false) {
 
-    constructor(_engine: WebGL2Engine, type: number, itemSize: number, data: number | Float32Array | Uint16Array | Uint8Array | Int8Array | Int16Array, usage: number = GL.STATIC_DRAW, indexArray: boolean = false) {
         super(_engine);
+        //针对矩阵的特殊处理
         let numColumns = 0;
         switch (type) {
             case GL.FLOAT_MAT4:
@@ -57,6 +111,7 @@ class WebGL2VertexBuffer extends WebGL2Object {
                 type = GL.FLOAT;
                 break;
         }
+        //计算数据长
         let dataLength = 0;
         let byteLength = 0;
         if (typeof data === "number") {
@@ -80,6 +135,7 @@ class WebGL2VertexBuffer extends WebGL2Object {
             dataLength = data.length;
             byteLength = data.byteLength;
         }
+        //设置
         this.type = type;
         this.itemSize = itemSize;
         this.numItems = type ? dataLength / (itemSize * numColumns) : byteLength / itemSize;
@@ -90,10 +146,14 @@ class WebGL2VertexBuffer extends WebGL2Object {
         this.indexArray = Boolean(indexArray);
         this.integer = Boolean(INTEGER_TYPES[this.type]);
         this.binding = this.indexArray ? GL.ELEMENT_ARRAY_BUFFER : GL.ARRAY_BUFFER;
+        //重制
         this.restore(data);
     }
-
-    public restore(data: number | Float32Array | Uint16Array | Uint8Array | Int8Array | Int16Array): WebGL2VertexBuffer {
+    /**
+     * 重制
+     * @param data 
+     */
+    public restore(data: WebGL2VertexBufferDataType): WebGL2VertexBuffer {
         if (!data) {
             data = this.byteLength;
         }
@@ -108,19 +168,24 @@ class WebGL2VertexBuffer extends WebGL2Object {
         gl.bindBuffer(this.binding, null);
         return this;
     }
-
-    public data(data: Float32Array | Uint16Array | Uint8Array): WebGL2VertexBuffer {
+    /**
+     * 设置数据
+     * @param data 
+     */
+    public data(data: WebGL2VertexBufferDataType): WebGL2VertexBuffer {
         const gl = this.gl;
         if (this.state.vertexArray) {
             gl.bindVertexArray(null);
             this.state.vertexArray = null;
         }
         gl.bindBuffer(this.binding, this.buffer);
-        gl.bufferSubData(this.binding, 0, data);
+        gl.bufferSubData(this.binding, 0, data as BufferSource);
         gl.bindBuffer(this.binding, null);
         return this;
     }
-
+    /**
+     * 删除
+     */
     public delete(): WebGL2VertexBuffer {
         if (this.buffer) {
             this.gl.deleteBuffer(this.buffer);
