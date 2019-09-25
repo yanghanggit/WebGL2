c4af7a3a3b4b33a35726a01092373388ede8de1c
yang hang <1220826563@qq.com>
2019-09-25 15:22:09 +0800

    添加摄像机的类

--- src/WebGL2DemoScene.ts
+++ src/WebGL2DemoScene.ts
@@ -1,5 +1,86 @@
 
 /**
+ * camera
+ */
+class Camera {
+    /**
+     * 
+     */
+    private readonly viewMatrix: Float32Array = mat4.create();
+    private readonly projMatrix: Float32Array = mat4.create();
+    private readonly viewProjMatrix: Float32Array = mat4.create();
+    /**
+     * 
+     */
+    private readonly eyePosition: Float32Array = vec3.fromValues(0, 0, 0);
+    /**
+     * 
+     */
+    private readonly center: Float32Array = vec3.fromValues(0, 0, 0);
+    /**
+     * 
+     */
+    private readonly up: Float32Array = vec3.fromValues(0, 1, 0);
+    /**
+     * 
+     */
+    private dirty: boolean;
+    /**
+     * 
+     * @param fovy 
+     * @param aspect 
+     * @param near 
+     * @param far 
+     */
+    constructor(fovy: number, aspect: number, near: number, far: number) {
+        //
+        this.projMatrix = mat4.create();
+        mat4.perspective(this.projMatrix, fovy, aspect, near, far); 
+        //
+        this.eye(1, 1, 1);
+        this.lookAt(0, 0, 0);
+        this.update();
+    }
+    /**
+     * 
+     * @param x 
+     * @param y 
+     * @param z 
+     */
+    public lookAt(x: number, y: number, z: number): Camera {
+        this.center[0] = x;
+        this.center[1] = y;
+        this.center[2] = z;
+        this.dirty = true;
+        return this;
+    }
+    /**
+     * 
+     * @param x 
+     * @param y 
+     * @param z 
+     */
+    public eye(x: number, y: number, z: number): Camera {
+        this.eyePosition[0] = x;
+        this.eyePosition[1] = y;
+        this.eyePosition[2] = z;
+        this.dirty = true;
+        return this;
+    }
+    /**
+     * 
+     */
+    public update(): Camera {
+        if (!this.dirty) {
+            return this;
+        }
+        this.dirty = false;
+        mat4.lookAt(this.viewMatrix, this.eyePosition, this.center, this.up);
+        mat4.multiply(this.viewProjMatrix, this.projMatrix, this.viewMatrix);
+        return this;
+    }
+}
+/**
  * 一个demo场景的基类
  */
 class WebGL2DemoScene {
